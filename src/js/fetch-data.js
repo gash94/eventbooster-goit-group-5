2a8514c97d1a62fd7161a5d8cbee67bc1c3ffdaa
MiloszPlesniak <105582642+MiloszPlesniak@users.noreply.github.com>
2022-11-18 20:14:45 +0100

    fix fetch

--- src/js/fetch-data.js
+++ src/js/fetch-data.js
@@ -3,7 +3,7 @@ const formSearch = inputSearch.parentNode
 
 export const fetchEvents = async (searchWord) => {
     try {
-        const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=GxDzkfGCFz900fvLiCUzjO4VEZhSzI0Z&keyword=${searchWord}&locale=*&size=20&page=1`)
+        const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=GxDzkfGCFz900fvLiCUzjO4VEZhSzI0Z&keyword=${searchWord}&locale=*&size=20&page=1`)
         const data = await response.json()
         console.log(data)
         return data

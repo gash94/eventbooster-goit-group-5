dbd9e69a2477672237b8b67bd1b59553bcdd5987
gash94 <104354207+gash94@users.noreply.github.com>
2022-11-18 22:24:15 +0100

    Update fetch-data.js

--- src/js/fetch-data.js
+++ src/js/fetch-data.js
@@ -1,37 +1,30 @@
 const axios = require('axios').default;
 
+const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events';
+const API_KEY = 'GxDzkfGCFz900fvLiCUzjO4VEZhSzI0Z';
 
+export async function fetchEvents(searchValue, country, page) {
+  try {
+    const response = await axios.get(`${BASE_URL}`, {
+      method: 'get',
+      params: {
+        apikey: API_KEY,
+        keyword: searchValue,
+        size: 20,
+        page: page,
+        countryCode: country,
+      },
+    });
 
-const inputSearch = document.querySelector("#searchEvent");
-const formSearch = inputSearch.parentNode
-
-
-const BASE_URL = "https://app.ticketmaster.com/discovery/v2/events";
-const API_KEY = "GxDzkfGCFz900fvLiCUzjO4VEZhSzI0Z"
-
-async function fetchEvents(searchValue, country, page) {
-    try {
-        const response = await axios.get(`${BASE_URL}`, {
-            method: "get",
-            params: {
-                apikey: API_KEY,
-                keyword: searchValue,
-                size: 20,
-                page: page,
-                countryCode: country,
-
-            }
-        })
-        console.log(response.data.page)
-        return {
-            events: response.data._embedded.events,
-            pageInfo: response.data.page
-        }
-
-    } catch (error) {
-        console.log(error)
-    }
+    return {
+      events: response.data._embedded.events,
+      pageInfo: response.data.page,
+    };
+  } catch (error) {
+    console.log(error);
+  }
 }
+
 // fetchEvents("rock", "", 1).then((response) => {
 
 //     response.events.map((item) => {

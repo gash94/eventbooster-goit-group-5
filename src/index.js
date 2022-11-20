38e248cafea2ccaade9f817502750f727f601c22
MiloszPlesniak <105582642+MiloszPlesniak@users.noreply.github.com>
2022-11-21 00:07:32 +0100

    add input search
    Dodałem całą logikę wyboru kraju wyszukiwania ostylowałem go i czeka na podpięcie do skryptu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import fetchEvents from './js/fetch-data';
 import renderCards from './js/render-cards';
 import markupEvents from './js/markup-event';
+import choseCountry from './js/chose-country';
---
+++ src/js/chose-country.js
@@ -0,0 +1,41 @@
+import renderCards from './render-cards';
+
+const inputSelectCountry = document.querySelector('#chose-country')
+const countryBox = document.querySelector('.country')
+const countries = [...document.querySelectorAll('.country__item')]
+const countryList = document.querySelector('.country__list')
+const inputSearch = document.querySelector('.search__input')
+const form = document.querySelector('form')
+
+
+
+inputSelectCountry.addEventListener('click', () => {
+    countryBox.classList.toggle('hide')
+    inputSelectCountry.classList.toggle('header__input--open')
+})
+
+countryBox.addEventListener('click', (e) => {
+    console.log(e.target.textContent)
+    inputSelectCountry.value = e.target.textContent
+    countryBox.classList.toggle('hide')
+    inputSelectCountry.classList.toggle('header__input--open')
+})
+inputSelectCountry.addEventListener('input', (e) => {
+    const currentWord = e.target.value.toUpperCase()
+    let result = countries
+    result = result.filter(countries => countries.textContent.toUpperCase().includes(currentWord))
+    countryList.textContent = ""
+    result.forEach(item => countryList.appendChild(item))
+})
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     renderCards(inputSearch.value, inputSelectCountry.value)
+//     console.log(inputSearch.value, inputSelectCountry.value)
+
+// })
+
+
+
+
+

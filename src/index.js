db73d0032e457898e4ec0edad37b17f7268fa055
gash94 <104354207+gash94@users.noreply.github.com>
2022-11-21 18:54:49 +0100

    Revert "add input search"
    This reverts commit 38e248cafea2ccaade9f817502750f727f601c22.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import fetchEvents from './js/fetch-data';
 import renderCards from './js/render-cards';
 import markupEvents from './js/markup-event';
-import choseCountry from './js/chose-country';

442d7b826968b64b72678c297802dcfd95e90357
BartekJedliczka <bartekjedliczka@gmail.com>
2022-11-24 17:18:43 +0100

    reverted fetch changes

--- src/js/fetch-data.js
+++ src/js/fetch-data.js
@@ -3,18 +3,18 @@ const axios = require('axios').default;
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events';
 const API_KEY = 'GxDzkfGCFz900fvLiCUzjO4VEZhSzI0Z';
 
-async function fetchEvents(keyword = '', country = '', page = 1) {
-  // TODO: Clean unused params from url
-  const queryParms = new URLSearchParams({
-    apikey: API_KEY,
-    keyword,
-    country,
-    page,
-  });
-
+async function fetchEvents(searchValue, country, page) {
   try {
-    const response = await axios.get(`${BASE_URL}?${queryParms}`);
-    console.log('response', response.data);
+    const response = await axios.get(`${BASE_URL}`, {
+      method: 'get',
+      params: {
+        apikey: API_KEY,
+        keyword: searchValue,
+        size: 20,
+        page: page,
+        countryCode: country,
+      },
+    });
     return {
       events: response.data._embedded.events,
       pageInfo: response.data.page,

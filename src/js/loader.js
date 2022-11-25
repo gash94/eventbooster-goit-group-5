eb839cec3336f9f7bd8de8df5b91d27af8c531fe
gash94 <104354207+gash94@users.noreply.github.com>
2022-11-25 17:01:35 +0100

    Update loader.js

--- src/js/loader.js
+++ src/js/loader.js
@@ -2,11 +2,11 @@ const preloader = document.querySelector('.preloader');
 
 export const fadeEffect = setInterval(() => {
   if (!preloader.style.opacity) {
-    preloader.style.opacity = 3;
+    preloader.style.opacity = 2;
   }
   if (preloader.style.opacity > 0) {
-    preloader.style.opacity -= 0.15;
-  } 
-}, 150);
-
-
+    preloader.style.opacity -= 0.05;
+  } else {
+    preloader.remove();
+  }
+}, 100);

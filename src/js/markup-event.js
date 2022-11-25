2ca0ca95d5d50d6a81ecc08e7621e76a2be1ff45
gash94 <104354207+gash94@users.noreply.github.com>
2022-11-25 16:33:48 +0100

    slice card name

--- src/js/markup-event.js
+++ src/js/markup-event.js
@@ -4,15 +4,16 @@ function markupEvents(eventItems) {
   const newCard = eventItems
     .map(card => {
       let nameEvent = card.name;
-      if (card.name.length > 20) {
-        nameEvent = card.name.slice(0, 20) + '...';
+      if (card.name.length > 17) {
+        nameEvent = card.name.slice(0, 17) + '...';
       }
-
-      return `<li class="cards__item  animate__animated animate__flipInX">
+      return `<li class="cards__item">
+      <div class="cards__box animate__animated animate__flipInX">
         <img class="cards__image" src="${card.urlImage}" alt="${card.name}" loading="lazy" />
         <h2 class="cards__title">${nameEvent}</h2>
         <p class="cards__date">${card.date}</p>
         <p class="cards__place"><span class="cards__pin">${card.place}</span></p>
+      </div>
     </li>`;
     })
     .join('');

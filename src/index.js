ba83b5d89c054f02ac436f99083232d328a11515
gash94 <104354207+gash94@users.noreply.github.com>
2022-11-26 13:55:25 +0100

    add spinner toggle

--- src/index.js
+++ src/index.js
@@ -5,6 +5,3 @@ import form from './js/chose-country';
 import fadeEffect from './js/loader';
 import modalTeam from './js/modal-team';
 import pagination from './js/pagination';
-import show from './js/spinner';
-
-  

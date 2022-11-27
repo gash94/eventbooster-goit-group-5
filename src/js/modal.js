7a6498a51e69a08365479c6d7e9b6045049ea752
gash94 <104354207+gash94@users.noreply.github.com>
2022-11-27 15:19:28 +0100

    update

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,5 +1,6 @@
-import { fetchEvent } from './fetch-data';
+import { fetchInfoEvent } from './fetch-data';
 import MicroModal from 'micromodal';
+import renderCards from './render-cards';
 const modalContentElem = document.querySelector('.modal__content');
 const modalTopImage = document.querySelector('.modal__top_image');
 const eventImageElem = document.querySelector('.event__image');
@@ -9,44 +10,46 @@ const placeElem = document.querySelector('.event__place');
 const whoElem = document.querySelector('.event__who');
 const pricesElem = document.querySelector('.event__prices');
 const cardsElem = document.querySelector('.cards');
-
-const apikey = "mkwIMojX5HHlKyGujroFjmMIYR083OUT";
+const moreAuthorBtn = document.querySelector('.event__btn2');
 
 MicroModal.init();
 
-cardsElem.addEventListener("click", event => {
+cardsElem.addEventListener('click', event => {
   event.preventDefault();
 
   MicroModal.show('modal-1');
-
+  console.log(event.target.dataset.id);
   updateModalData(event.target.dataset.id);
 });
 
-
-async function updateModalData(eventId) {
-  // const eventId = event.target.dataset.id;
-
-  // const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events/${eventId}.json?apikey=${apikey}`);
-  const eventData = await fetchEvent(eventId);
-  
+export async function updateModalData(eventId) {
+  const eventData = await fetchInfoEvent(eventId);
 
   console.log(eventData);
 
+  let nameArtist = eventData.name;
+  moreAuthorBtn.dataset.name = nameArtist;
   modalTopImage.innerHTML = '';
   eventImageElem.innerHTML = '';
   pricesElem.innerHTML = '';
   placeElem.innerHTML = '';
 
-  modalTopImage.insertAdjacentHTML("beforeend", `
+  modalTopImage.insertAdjacentHTML(
+    'beforeend',
+    `
     <img src="${eventData.images[4].url}">
-  `);
+  `
+  );
 
-  eventImageElem.insertAdjacentHTML("beforeend", `
+  eventImageElem.insertAdjacentHTML(
+    'beforeend',
+    `
     <img src="${eventData.images[4].url}">
-  `)
+  `
+  );
 
-
-  infoElem.textContent = eventData.info || eventData.description || eventData.name;
+  infoElem.textContent =
+    eventData.info || eventData.description || eventData.name;
   startDateElem.innerHTML = `
     ${eventData.dates.start.localDate}
     <br>
@@ -55,25 +58,28 @@ async function updateModalData(eventId) {
 
   whoElem.textContent = eventData.name;
 
-  for(const venue of eventData._embedded.venues) {
-    placeElem.insertAdjacentHTML("beforeend", `
+  for (const venue of eventData._embedded.venues) {
+    placeElem.insertAdjacentHTML(
+      'beforeend',
+      `
     ${venue.city.name}, ${venue.country.name}
     <br>
     ${venue.name}
-    `)
+    `
+    );
   }
 
-  for(const priceRange of eventData.priceRanges) {
-    pricesElem.insertAdjacentHTML("beforeend", `
+  for (const priceRange of eventData.priceRanges) {
+    pricesElem.insertAdjacentHTML(
+      'beforeend',
+      `
       <div class="event__single_price">
         <div>
           ${priceRange.type} ${priceRange.min}-${priceRange.max} ${priceRange.currency}
         </div>
         <a class="event__btn1 event__buy_tickets" href="${eventData.url}" target="_blank">BUY TICKETS</a>
       </div>
-    `)
+    `
+    );
   }
-
 }
-
-
